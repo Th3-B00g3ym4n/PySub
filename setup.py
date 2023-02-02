737ad0075a7021725cbe84776ddf486bb7d3ff82
Th3-B00g3ym4n <119944504+Th3-B00g3ym4n@users.noreply.github.com>
2023-02-02 08:20:23 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pysub',
+    version="dev",
+
+    url='https://github.com/Th3-B00g3ym4n/PySub',
+    author='The_Boogeyman',
+    author_email='HAHAHA',
+
+    py_modules=['pysub'],
+)
